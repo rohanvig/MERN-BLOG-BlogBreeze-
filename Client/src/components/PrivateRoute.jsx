4094d376d638b3c7e8f23b5e8ace131be413bd55
rohan <rohanvig777@gmail.com>
2024-08-11 23:38:50 +0530

    dashboard private

--- Client/src/components/PrivateRoute.jsx
+++ Client/src/components/PrivateRoute.jsx
@@ -1,8 +1,7 @@
-import { current } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
-  const {currentUser}=useSelector((state)=>state.user);
-    return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }

2b9b5d3287b30e192d1bd2e09b059460f6050d80
rohan <rohanvig777@gmail.com>
2024-08-14 02:04:42 +0530

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -4,10 +4,10 @@ export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(400, "you are not allowed to create a post"));
   }
-  if (!req.body.title || !req.body.content) {
+  if (!req.body.category || !req.body.content) {
     return next(errorHandler(400, "Please provide all required Fields"));
   }
-  const slug = req.body.title
+  const slug = req.body.category
     .split(" ")
     .join("-")
     .toLowerCase()
@@ -25,4 +25,48 @@ export const create = async (req, res, next) => {
     next(error)
   }
 };
+
+
+// searching posts (fetching the posts)
+export const getposts=async(req,res,next)=>{
+  try{
+const startIndex=parseInt(req.query.startIndex) || 0;
+const limit=parseInt(req.query.userId);
+const sortDirection=req.query.order === 'asc' ? 1 : -1;
+const posts=await Post.find(
+  {
+    ...(req.query.userId && {userId:req.query.userId}),
+    ...(req.query.category && {category:req.query.category}),
+    ...(req.query.slug && {category:req.query.slug}),
+    ...(req.query.postId && {category:req.query.postId}),
+    ...(req.query.searchTerm && {
+      $or:[
+        {title:{$regex:req.query.searchTerm,$options:'i'}},
+        {content:{$regex:req.query.searchTerm,$options:'i'}},
+      ],
+    }),
+  }
+).sort({createdAt:sortDirection}).skip(startIndex).limit(limit)
+const totalPosts=await Post.countDocuments();
+const now=new Date();
+const oneMonthAgo=new Date(
+  now.getFullYear(),
+  now.getMonth()-1,
+  now.getDate()
+);
+const lastMonthPosts=await Post.countDocuments({
+  createdAt:{
+    $gte:oneMonthAgo
+    }
+})
+res.status(200).json({
+  posts,
+  totalPosts,
+  lastMonthPosts
+});
+  }
+  catch(error){
+    next(error);
+  }
+}
 

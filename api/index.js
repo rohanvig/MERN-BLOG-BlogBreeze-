89a3724bed9b1d52b4ee14743d753c0fde6183de
rohan <rohanvig777@gmail.com>
2024-08-14 00:08:42 +0530

    Post api created

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import postRoutes from "./routes/post.route.js";
+
 
 dotenv.config();
 mongoose
@@ -21,8 +23,9 @@ app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 
 //Middleware for error handling
 

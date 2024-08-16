645de851b7a4bbbeed147dae1722d1f48d189ad6
rohan <rohanvig777@gmail.com>
2024-08-16 11:59:16 +0530

    add dashboard ov erview to the admin

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -59,64 +59,71 @@ export const likeComment = async (req, res, next) => {
   }
 };
 
-export const editComment =async (req,res,next)=>{
-  try{
+export const editComment = async (req, res, next) => {
+  try {
     const comment = await Comment.findById(req.params.commentId);
-    if(!comment){ 
+    if (!comment) {
       return next(errorHandler(404, "Comment not found"));
     }
-    if(comment.userId !== req.user.id && !req.user.isAdmin){
-      return next(errorHandler(403, "You are not authorized to edit this comment"));
-      }
-      const editedComment=await Comment.findByIdAndUpdate(
-        req.params.commentId,
-        {
-          content:req.body.content
-        },
-        {new:true}
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not authorized to edit this comment")
       );
-      res.status(200).json(editedComment);
-  }catch(error){
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    res.status(200).json(editedComment);
+  } catch (error) {
     next(error);
   }
-}
+};
 
-export const deleteComment=async(req,res,next)=>{
-  try{
+export const deleteComment = async (req, res, next) => {
+  try {
     const comment = await Comment.findById(req.params.commentId);
-    if(!comment){
+    if (!comment) {
       return next(errorHandler(404, "Comment not found"));
     }
-    if(comment.userId !== req.user.id && req.user.isAdmin){
-      return next(errorHandler(403, "You are not authorized to delete this comment"));
+    if (comment.userId !== req.user.id && req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not authorized to delete this comment")
+      );
     }
     await Comment.findByIdAndDelete(req.params.commentId);
-    res.status(200).json('Comment has been deleted');
-  }
-  catch(error){
+    res.status(200).json("Comment has been deleted");
+  } catch (error) {
     next(error);
   }
-}
+};
 
-export  const getcomments=async(req,res,next)=>{
-  if(!req.user.isAdmin){
-    return next(errorHandler(403,'You are not allowed to get all comments'))
-  }
-  try{
-    const startIndex=parseInt(req.query.startIndex) || 0;
-    const limit=parseInt(req.query.limit) || 9;
-    const sortDirection=req.query.sort === 'desc'?-1:1;
-    const comments=await Comment.find().sort({createdAt:sortDirection}).skip(startIndex).limit(limit);
-    const totalComments=await Comment.countDocuments();
-    const now =new Date();
-    const oneMonthAgo=new Date(now.getFullYear().now.getMonth()-1,now.getDate());
-    const lastMonthComments=await Comment.countDocuments({createdAt:{$gte:oneMonthAgo}});
-    res.status(200).json({comments,totalComments,lastMonthComments});
-
-
-
-  }
-  catch(error){
+export const getcomments = async (req, res, next) => {
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not allowed to get all comments"));
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "desc" ? -1 : 1;
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
+  } catch (error) {
     next(error);
   }
-}
+};

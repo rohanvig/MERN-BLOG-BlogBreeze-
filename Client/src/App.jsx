dee98ff45666bb25781a1a02a9b89ad8f6770bc7
rohan <rohanvig777@gmail.com>
2024-09-04 15:28:02 +0530

    add some updations

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import SignUp from "./pages/SignUp";
+import SignUp from "./pages/Signup";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";

71b50f60d42f324ade3197db5da1cc0f87ff711a
rohan <rohanvig777@gmail.com>
2024-09-07 21:45:44 +0530

    config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables for the current mode (development, production, etc.)
+  // Explicitly load env variables
   const env = loadEnv(mode, process.cwd());
 
   return {
     server: {
-    //   proxy: {
-    //     '/api': {
-    //       target: env.VITE_BACKEND_URL, // Use `VITE_` prefix for env vars in Vite
-    //       changeOrigin: true,             // Ensure that the origin is rewritten
-    //       secure: false,                  // If using HTTPS on the target server
-    //     },
-    //   },
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL, // Using the loaded environment variable
+          secure: false,
+        },
+      },
     },
     plugins: [react()],
   };
